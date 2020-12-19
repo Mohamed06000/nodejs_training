2993af1a09503c775deb80db99fffdd7afb77105
Mohamed Mazy <mazymohamed06@gmail.com>
2020-12-19 18:35:34 +0000

    server created

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+let http = require('http')
+let fs = require('fs')
+
+let server = http.createServer()
+server.on('request', (request, response) => {
+    fs.readFile('indeex.html', (err, data) => {
+        if (err) {
+            response.writeHead(404)
+            response.end('Fichier n\'existe pas !')
+        }
+        response.writeHead(200)
+        response.end(data)
+
+    })
+
+})
+
+server.listen(3001)
